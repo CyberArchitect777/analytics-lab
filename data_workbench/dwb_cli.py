a85415405e8d68022db3b1cedf7c50774f1871ce
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2025-08-10 20:25:09 +0100

    chore: remove help option from CLI options as it is implied

--- data_workbench/dwb_cli.py
+++ data_workbench/dwb_cli.py
@@ -19,7 +19,6 @@ def help() -> None:
     print("--first-x-records = Display the first x records of the input file")
     print("--remove-columns-by-name = Remove columns from the input file by name")
     print("--remove-records-by-index = Remove records by start index and number of records")
-    print("--help = Help\n")
 
 def main() -> None:
     
