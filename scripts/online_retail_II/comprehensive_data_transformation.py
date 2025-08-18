e89ca6e502eea7ba83876f87c117322801da4ece
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2025-08-18 12:39:05 +0000

    add a new script designed to show a comprehensive data transformation

---
+++ scripts/online_retail_II/comprehensive_data_transformation.py
@@ -0,0 +1,38 @@
+# An example of a data transformation using a variety of tools
+
+import pandas as pd
+from pathlib import Path
+
+def start_transform() -> None:
+	# Input Excel
+	#print("Read Excel file...")
+	#df1 = pd.read_excel("../../data/online_retail_II.xlsx", sheet_name=0)
+	#df2 = pd.read_excel("../../data/online_retail_II.xlsx", sheet_name=1)
+	# Input CSV
+	print("Read CSV file...")
+	df_joined = pd.read_csv("../../outputs/csv/online_retail_II_combined.csv", encoding="utf-8")
+	# Print Excel sheet sizes in rows and columns
+	#print("Excel 1 Rows, columns" + str(df1.shape))
+	#print("Excel 2 Rows, columns" + str(df2.shape))
+	# Combine the two Excel sheets into one dataset
+	#df_joined = pd.concat([df1, df2])
+	# Print combined dataset size in rows and columns
+	print("CSV Rows, columns" + str(df_joined.shape))
+	# Output file to CSV
+	#print("Write CSV...")
+	#df_joined.to_csv("online_retail_II.csv", index=False, encoding="utf-8", lineterminator="\n")
+	# Write out a dataset table listing to html
+	print("Output dataset table to HTML")
+	output_dataset_to_html(df_joined, "../../outputs/html/preview.html")
+	print("Output column statistical data for numerical fields")
+	print(df_joined.describe().to_string())
+	print("Show data columns and types")
+	print(df_joined.dtypes)
+	
+def output_dataset_to_html(df: pd.DataFrame, filename: str) -> None:
+	# Simple HTML export of the provided dataset
+	html_str = df.head(1000).to_html(index=False, border=0)
+	Path(filename).write_text(html_str, encoding="utf-8")
+	
+if __name__ == "__main__":
+	start_transform()
