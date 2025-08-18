555eb8413a0536e976d665eb71a484a448767bdf
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2025-08-18 21:51:16 +0000

    add print statements to Excel read path and ensure it all runs

--- scripts/online_retail_II/comprehensive_data_transformation.py
+++ scripts/online_retail_II/comprehensive_data_transformation.py
@@ -5,22 +5,22 @@ from pathlib import Path
 
 def start_transform() -> None:
 	# Input Excel
-	#print("Read Excel file...")
-	#df1 = pd.read_excel("../../data/online_retail_II.xlsx", sheet_name=0)
-	#df2 = pd.read_excel("../../data/online_retail_II.xlsx", sheet_name=1)
+	print("Stage 1 - Read Excel file...")
+	df1 = pd.read_excel("../../data/online_retail_II/online_retail_II.xlsx", sheet_name=0)
+	df2 = pd.read_excel("../../data/online_retail_II/online_retail_II.xlsx", sheet_name=1)
 	# Input CSV
-	print("Stage 1 - Read CSV file...")
-	df_joined = pd.read_csv("../../outputs/csv/online_retail_II_combined.csv", encoding="utf-8")
+	#print("Stage 1 - Read CSV file...")
+	#df_joined = pd.read_csv("../../outputs/csv/online_retail_II_combined.csv", encoding="utf-8")
 	# Print Excel sheet sizes in rows and columns
-	#print("Excel 1 Rows, columns" + str(df1.shape))
-	#print("Excel 2 Rows, columns" + str(df2.shape))
+	print("Stage 2a - Excel 1 Rows, columns" + str(df1.shape))
+	print("Stage 2b - Excel 2 Rows, columns" + str(df2.shape))
 	# Combine the two Excel sheets into one dataset
-	#df_joined = pd.concat([df1, df2])
+	df_joined = pd.concat([df1, df2])
 	# Print combined dataset size in rows and columns
-	print("Stage 2 - Rows/columns - " + str(df_joined.shape))
+	#print("Stage 2 - Rows/columns - " + str(df_joined.shape))
 	# Output file to CSV
-	#print("Write CSV...")
-	#df_joined.to_csv("online_retail_II.csv", index=False, encoding="utf-8", lineterminator="\n")
+	print("Stage 2c - Write CSV...")
+	df_joined.to_csv("online_retail_II.csv", index=False, encoding="utf-8", lineterminator="\n")
 	# Write out a dataset table listing to html
 	print("Stage 3 - Output dataset table to HTML")
 	output_dataset_to_html(df_joined, "../../outputs/html/preview.html")
