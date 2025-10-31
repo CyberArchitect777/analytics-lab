4f41f8eeb5634cb06a9e92b4bf468166c1978f92
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2025-10-31 19:46:11 +0000

    create new script for the generated 6gb dataset in order to demonstrate the handling of very large csv files

---
+++ scripts/generated_6gb_dataset/large_data_transformation.py
@@ -0,0 +1,36 @@
+# An example of a data transformation techniques on a very large dataset
+# requiring efficient memory usage.
+	
+import os
+import pandas as pd
+import random
+import string
+from datetime import datetime, timedelta
+
+def generate_dataset():
+	if not os.path.exists("../../outputs/csv/generated_big_file.csv"):
+		total_rows = 150000000
+		chunk_size = 1000000
+		current_chunk = 0
+		while current_chunk <= total_rows:
+			print("* Creating record " + str(current_chunk+1) + " to " + str(current_chunk + 1000000))
+			big_data = {
+		    	"AccountCode": [random.randint(10000, 99999) for _ in range(chunk_size)],
+		    	"TransactionID": [i + 1 + chunk_size for i in range(chunk_size)],
+		    	"Net": [round(random.uniform(0, 100), 2) for _ in range(chunk_size)],
+		    	# _ is a placeholder, not an accessible variable
+		    	"Date": [
+		    (datetime(2025, 1, 1) + timedelta(days=random.randint(0, 364))).strftime("%Y-%m-%d")
+		    for _ in range(chunk_size)],
+		    	"Reference": ["".join(random.choices(string.ascii_uppercase + string.digits, k=8)) for _ in range(chunk_size)],
+		}
+			chunk_dataframe = pd.DataFrame(big_data)
+			if current_chunk == 0:
+				chunk_dataframe.to_csv("../../outputs/csv/generated_big_file.csv", index=False, encoding="utf-8", lineterminator="\n", header=True)
+			else:
+				chunk_dataframe.to_csv("../../outputs/csv/generated_big_file.csv", mode="a", index=False, encoding="utf-8", lineterminator="\n", header=False)
+			current_chunk += 1000000
+
+if __name__ == "__main__":
+	print("Stage 1 - Generate 6GB dataset for large file testing and write to CSV (if file doesn't exist)")
+	generate_dataset()
